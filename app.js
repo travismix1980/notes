8c155155bbb8ef31faec8df05ed5205777ec939e
Travis Mix <travismix1980@gmail.com>
2020-09-27 02:27:10 -0700

    did some basic validator testing and figuring out how to get intellesense working with npm packages

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const notes = require('./notes');
+const chalk = require('chalk');
 
 const msg = notes.getNotes();
 
 console.log(msg);
+console.log(chalk.green('Success!'));
+console.log(chalk.bgBlue.red.bold('Hello World!'));

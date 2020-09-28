a7c2d99179a037ed09be8e8e0a967aeaaccc868f
Travis Mix <travismix1980@gmail.com>
2020-09-28 12:44:24 -0700

    added yargs and started setting up our yargs commands

--- app.js
+++ app.js
@@ -1,7 +1,58 @@
 const notes = require('./notes');
 const chalk = require('chalk');
+const yargs = require('yargs');
+const fs = require('fs');
 
-const msg = notes.getNotes();
+// add, remove, read, list
 
-console.log(msg);
-console.log(chalk.blue('Success!'));
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'note title',
+            demandOption: true,
+            type: 'string',
+        },
+        body: {
+            describe: 'The notes body',
+            demandOption: true,
+            type: 'string',
+        },
+    },
+    handler: function(argv){
+        console.log('Adding a new note!');
+        console.log(`Title: ${argv.title}`);
+        console.log(`Body: ${argv.body}`);
+    }
+});
+
+// create a remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log('Removing a note');
+    }
+});
+
+// create a read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function() {
+        console.log('Reading a note');
+    }
+});
+
+// create a list command
+yargs.command({
+    command: 'list',
+    describe: 'List the notes',
+    handler: function(){
+        console.log('Listing notes');
+    }
+})
+
+yargs.parse();

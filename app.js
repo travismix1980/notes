9a0365217ed5ea2f5ffdeae3675a097eea89c85c
Travis Mix <travismix1980@gmail.com>
2020-09-27 22:44:07 -0700

    final commit for 9/27/2020

--- app.js
+++ app.js
@@ -4,6 +4,4 @@ const chalk = require('chalk');
 const msg = notes.getNotes();
 
 console.log(msg);
-console.log(chalk.green('Success!'));
-console.log(chalk.bgBlue.red.bold('Hello World!'));
-console.log(chalk.green('I am a green line ' + chalk.blue.underline.bold('with a blue substring') + ' that becomes green again!'));
+console.log(chalk.blue('Success!'));

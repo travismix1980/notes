ff410767d73bfa17d999eeeef560e94a6048eb6c
Travis Mix <travismix1980@gmail.com>
2020-09-29 17:56:05 -0700

    read note by title functionality added app complete

--- notes.js
+++ notes.js
@@ -1,51 +1,68 @@
 const fs = require('fs');
 const chalk = require('chalk');
 
-const getNotes = function(){
-    return 'Your notes...';
-}
-
-const addNote = function(title, body){
+const addNote = function (title, body) {
     const notes = loadNotes();
-    const duplicateNotes = notes.filter(function(note){
-        return note.title === title;
-    });
+    const duplicateNote = notes.find((note) => note.title === title);
 
-    if(duplicateNotes.length === 0){
+    if (!duplicateNote) {
         notes.push({
             title,
             body,
         });
         saveNotes(notes);
-        console.log(chalk.green.inverse('New note added'));
-    }else{
-        console.log(chalk.red.inverse('Note title already used please rewrite note with a different title'));
+        console.log(chalk.green('New note added'));
+    } else {
+        console.log(chalk.red('Note title already used please rewrite note with a different title'));
     }
 }
-const removeNote = function(title){
+const removeNote = (title) => {
     const notes = loadNotes();
-    const modifiedNotes = notes.filter(function(note){
+    const modifiedNotes = notes.filter((note) => {
         return note.title !== title;
     });
-    if(notes.length === modifiedNotes.length){
-        console.log(chalk.red.inverse('No matching note found to be removed'));
-    } else{
-        console.log(chalk.green.inverse('Note Removed'));
+    if (notes.length === modifiedNotes.length) {
+        console.log(chalk.red('No matching note found to be removed'));
+    } else {
+        console.log(chalk.green('Note Removed'));
         saveNotes(modifiedNotes);
     }
 }
 
-const saveNotes = function(notes){
+const listNotes = () => {
+    const notes = loadNotes();
+    if (notes.length > 0) {
+        console.log(chalk.green('Displaying note titles'));
+        notes.forEach((note) => {
+            console.log(chalk.whiteBright(note.title));
+        });
+    } else {
+        console.log(chalk.red('No note titles to display'));
+    }
+}
+
+const readNote = (title) => {
+    const notes = loadNotes();
+    const note = notes.find((note) => note.title === title);
+    if (note) {
+        console.log(chalk.green(note.title));
+        console.log(note.body);
+    } else {
+        console.log(chalk.red('No matching note found'));
+    }
+}
+
+const saveNotes = (notes) => {
     fs.writeFileSync('notes.json', JSON.stringify(notes));
 }
 
-const loadNotes = function(){
-    try{
+const loadNotes = () => {
+    try {
         return JSON.parse(fs.readFileSync('notes.json').toString());
-    }catch(e){
+    } catch (e) {
         return [];
     }
 
 }
 
-module.exports = {getNotes, addNote, removeNote};
+module.exports = {addNote, removeNote, listNotes, readNote};
